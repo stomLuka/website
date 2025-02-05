26dc803655af4d6566d2bb15345aea6db38423d6
stomIuka <hlyupa15@mail.ru>
2025-02-05 20:33:28 +0500

    seo+fix-commit

--- src/components/PersonnelPage/PersonnelMain.jsx
+++ src/components/PersonnelPage/PersonnelMain.jsx
@@ -5,42 +5,49 @@ const PersonnelMain = () => {
     const personnel = [
         {
             name: "Богданова Любовь Ивановна",
+            role: "стоматолог терапевт",
             education: "высшее, диплом Красноярского медицинского института, 1993г ШВ №093576",
             accreditation: "7723031495898",
             services: "стоматология терапевтическая",
         },
         {
             name: "Богданов Александр Анатольевич",
+            role: "стоматолог ортопед",
             education: "высшее, диплом Красноярского медицинского института, 1993г НТВ №093601",
             accreditation: "772303222479",
             services: "стоматология ортопедическая",
         },
         {
             name: "Бунчук Ксения Николаевна",
+            role: "стоматолог терапевт",
             education: "высшее, диплом Красноярского медицинского академия, 2006г ВСГ 0139642",
             accreditation: "7722029826022",
             services: "стоматология терапевтическая, высшая категория",
         },
         {
             name: "Коротаев Юрий Владимирович",
+            role: "стоматолог",
             education: "высшее, диплом Красноярского медицинского университета, 2018г 102431 0111166",
             accreditation: "7723030257163",
             services: "стоматология",
         },
         {
             name: "Петров Михаил Андреевич",
+            role: "стоматолог хирург",
             education: "высшее, диплом Красноярского медицинского института, 1992г ФВ №029819",
             accreditation: "7723031500332",
             services: "стоматология хирургическая, высшая категория",
         },
         {
             name: "Родина Ирина Владиславовна",
+            role: "стоматолог терапевт",
             education: "высшее, диплом Красноярского медицинского института ,1993г TUB №094186",
             accreditation: "7723031610421",
             services: "стоматология терапевтическая, высшая категория",
         },
         {
             name: "Тарасов Сергей Владимирович",
+            role: "стоматолог ортопед",
             education: "высшее, диплом Красноярского медицинского института, 1992г ФВ №029733",
             accreditation: "7723028498265",
             services: "стоматология ортопедическая",
@@ -53,12 +60,12 @@ const PersonnelMain = () => {
                 ↑
             </HashLink>
             <div className="main-container">
-                <h1 className="personnel-title">Наш персонал</h1>
+                <h1 className="personnel-title">Наши врачи</h1>
 
                 <div className="personnel-container">
                     {personnel.map((person, index) => (
                         <div key={index} className="person-container">
-                            <h2 className="person-name">{person.name}</h2>
+                            <h2 className="person-name">{person.name} - {person.role}</h2>
                             <ul className="person-text">
                                 <li className="person-text-el">
                                     <strong>Образование:</strong> {person.education}
@@ -78,4 +85,4 @@ const PersonnelMain = () => {
     )
 }
 
-export default PersonnelMain
+export default PersonnelMain;
